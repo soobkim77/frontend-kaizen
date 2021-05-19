1989712b7e18f6e551061d8cadb93472bc2decb4
Soo-Beom <soobeomkim77@gmail.com>
2021-05-19 11:11:02 -0700

    signup front end design/functionality works

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import SignIn from './components/SignIn'
+import SignUp from './components/SignUp'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <SignIn/>
+      <SignUp/>
     </div>
   );
 }
